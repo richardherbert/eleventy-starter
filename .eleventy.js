478172814fe3b9c91617afde7dd4e217fb0f5977
Richard Herbert <richard@infoweb.co.uk>
2020-11-12 01:57:59 +0000

    Change assets path

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function( eleventyConfig ) {
 	eleventyConfig.setQuietMode( true );
 	eleventyConfig.setTemplateFormats( 'html,md' );
 
-	eleventyConfig.addPassthroughCopy( './src/assets/' );
+	eleventyConfig.addPassthroughCopy( './assets/' );
 
 	eleventyConfig.addFilter( 'dump', function( anything ) {
 		console.log( 'dump:', anything );
